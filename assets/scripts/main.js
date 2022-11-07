7c508cba0ba958aa5adfb400bd19e81a41d581c2
Miko <mikobrown135@gmail.com>
2022-11-06 22:15:18 -0800

    Fixing prevent default

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -73,6 +73,7 @@ function initFormHandler() {
   // B3. TODO - Add an event listener for the 'submit' event, which fires when the
   //            submit button is clicked
   formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
     const formData = new FormData(formEl);
     let recipeObject = {};
     for (const key of formData.keys()) {
